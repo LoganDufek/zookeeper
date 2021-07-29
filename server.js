40bc40df3cb453dfcdcf2dc6c17e9b4e017c540d
Logan D <logandufek@gmail.com>
2021-07-28 21:09:31 -0400

    Heroku push

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const fs = require('fs');
 const path = require('path');
 
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 
@@ -120,6 +121,22 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
     }
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
